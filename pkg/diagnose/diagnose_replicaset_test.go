a82909d0bc9fafc34ecc63744ce604dc91f7086c
Xavier Coulon <xcoulon@redhat.com>
2022-11-01 19:44:48 +0100

    feat(diagnose): check ReplicaSet when no Pod matching Service labels (#21)
    when there is no Pod matching the Service labels, attempt to lookup the
    "parent" ReplicaSet and check its status.
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/diagnose/diagnose_replicaset_test.go
+++ pkg/diagnose/diagnose_replicaset_test.go
@@ -13,7 +13,7 @@ import (
 
 var _ = Describe("diagnose replicasets", func() {
 
-	It("should detect sa not found", func() {
+	It("should detect sa not found from replicaset", func() {
 		// given
 		logger := logr.New(io.Discard)
 		apiserver, err := NewFakeAPIServer(logger, "resources/replicaset-service-account-not-found.yaml")
@@ -26,7 +26,24 @@ var _ = Describe("diagnose replicasets", func() {
 		// then
 		Expect(err).NotTo(HaveOccurred())
 		Expect(found).To(BeTrue())
-		Expect(logger.Output()).To(ContainSubstring(`👀 checking ReplicaSet 'sa-notfound'...`))
+		Expect(logger.Output()).To(ContainSubstring(`👀 checking replicaset 'sa-notfound'...`))
+		Expect(logger.Output()).To(ContainSubstring(`👻 replicaset 'sa-notfound' failed to create pods: pods "sa-notfound-" is forbidden: error looking up service account test/sa-notfound: serviceaccount "sa-notfound" not found`))
+	})
+
+	It("should detect sa not found from route", func() {
+		// given
+		logger := logr.New(io.Discard)
+		apiserver, err := NewFakeAPIServer(logger, "resources/replicaset-service-account-not-found.yaml")
+		Expect(err).NotTo(HaveOccurred())
+		cfg := NewConfig(apiserver.URL, "/api")
+
+		// when
+		found, err := diagnose.Diagnose(logger, cfg, diagnose.Route, "default", "sa-notfound")
+
+		// then
+		Expect(err).NotTo(HaveOccurred())
+		Expect(found).To(BeTrue())
+		Expect(logger.Output()).To(ContainSubstring(`👀 checking replicaset 'sa-notfound'...`))
 		Expect(logger.Output()).To(ContainSubstring(`👻 replicaset 'sa-notfound' failed to create pods: pods "sa-notfound-" is forbidden: error looking up service account test/sa-notfound: serviceaccount "sa-notfound" not found`))
 	})
 
