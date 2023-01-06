e3cf75e17767001ec3882d8f59c9d4288d56d65e
Xavier Coulon <xcoulon@redhat.com>
2023-01-06 07:06:26 +0100

    refactor(diagnose): improve message when no culprit is found (#65)
    also, refactor funcs to move logic out of the `/cmd` pkg 
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/diagnose/diagnose.go
+++ pkg/diagnose/diagnose.go
@@ -5,10 +5,24 @@ import (
 
 	"github.com/xcoulon/kubectl-diagnose/pkg/logr"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/rest"
 )
 
 func Diagnose(logger logr.Logger, cfg *rest.Config, kind ResourceKind, namespace, name string) (bool, error) {
+	found, err := diagnose(logger, cfg, kind, namespace, name)
+	switch {
+	case apierrors.IsNotFound(err):
+		// if resource was not found, just print the error but
+		// no need to print of the cmd usage
+		logger.Errorf(err.Error())
+	case !found:
+		logger.Infof(notFoundMsg)
+	}
+	return found, err
+}
+
+func diagnose(logger logr.Logger, cfg *rest.Config, kind ResourceKind, namespace, name string) (bool, error) {
 	switch kind {
 	case Route:
 		return diagnoseRoute(logger, cfg, namespace, name)
@@ -30,3 +44,10 @@ func Diagnose(logger logr.Logger, cfg *rest.Config, kind ResourceKind, namespace
 		return false, fmt.Errorf("🤷 unsupported kind of resource: '%s'", kind)
 	}
 }
+
+const notFoundMsg = `🤷 couldn't find the culprit
+possible causes:
+- invalid configuration of a sidecar container or a proxy within the pod
+- trying to connect to a container which is listening to '127.0.0.1' instead of '0.0.0.0'
+- something else?
+`
